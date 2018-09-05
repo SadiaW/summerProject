fc332c0c3c18812655cba13b01e4d105e0ec966a
SadiaW <41970862+SadiaW@users.noreply.github.com>
2018-09-05 00:39:44 -0400

    Updated files for fashion website

---
+++ ss.js
@@ -0,0 +1,35 @@
+function openPage(pageName,elmnt,color) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].style.backgroundColor = "";
+    }
+    document.getElementById(pageName).style.display = "block";
+    elmnt.style.backgroundColor = color;
+}
+
+window.onload = function () {
+  startTab();
+  carousel();
+};
+
+function startTab() {
+    document.getElementById("defaultOpen").click();
+}
+
+var myIndex = 0;
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+       x[i].style.display = "none";
+    }
+    myIndex++;
+    if (myIndex > x.length) {myIndex = 1}
+    x[myIndex-1].style.display = "block";
+    setTimeout(carousel, 3000); // Change image every 3 seconds
+}
